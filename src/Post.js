89abe8d8bcf53c7597b3d8fbb9a6f82d378d2893
chaeikim <sb02348@hanmail.net>
2023-10-08 11:37:26 +0900

    layout

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+    <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1696395050055-eb7a315bb1cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Chaei Kim</a>
+            <time>2023-01-06 16:32</time>
+          </p>
+          <p className="summary">Lorem ipsum dolor. Provident, numquam! Voluptatibus, vero corporis! Libero reprehenderit, labore voluptate eveniet ipsam unde, dolorem eligendi sequi aut aspernatur at accusantium doloribus optio vitae.</p>
+        </div>
+    </div>
+    )
+}
